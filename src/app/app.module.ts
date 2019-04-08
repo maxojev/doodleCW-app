1a5667f73200b8e49abdd57d7009284510c61d82
Maxime CHABI <maxime.chabi@kurmi-software.com>
2019-04-08 10:35:34 +0200

    start projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormUserComponent } from './form-user/form-user.component';
+
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormUserComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
